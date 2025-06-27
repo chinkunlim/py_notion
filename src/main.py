627cbb54a1f61433769f7c77aead8756a89de81a
chinkunlim <411383025@gms.ndhu.edu.tw>
2025-06-27 14:21:06 +0800

    Test read config.ini

--- src/main.py
+++ src/main.py
@@ -13,6 +13,5 @@ def main():
     logger.error("error")
     logger.warning("warning")
 
-
 if __name__ == "__main__":
     main()
--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -1,12 +1,29 @@
 from src.config import config
 
 def test_read_config():
-    print("Read 'Logging' ")
-    print(f"log_folder   : {config.get_config("log_folder", section="Logging", default="")}")
-    print(f"log_filename : {config.get_config("log_filename", section="Logging", default="")}")
-    print(f"log_level.   : {config.get_config("log_level", section="Logging", default="")}")
-    print(f"log_format   : {config.get_config("log_format", section="Logging", default="")}")
-    print(f"log_encoding : {config.get_config("log_encoding", section="Logging", default="")}")
+    log_folder = config.get_config("log_folder", section="Logging", default="")
+    log_filename = config.get_config("log_filename", section="Logging", default="")
+    log_level = config.get_config("log_level", section="Logging", default="")
+    log_format = config.get_config("log_format", section="Logging", default="")
+    log_encoding = config.get_config("log_encoding", section="Logging", default="")
+
+    print("Read 'Logging'")
+    print(log_encoding)
+    print(log_filename)
+    print(log_folder)
+    print(log_format)
+    print(log_level)
+
+    """ 
+    assert log_folder == "logs"
+    assert log_filename == "app.log"
+    assert log_level == "DEBUG"
+    assert log_format == "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    assert log_encoding == "utf-8"
+    """
+
+def main():
+    test_read_config()
 
 if __name__ == "__main__":
-    test_read_config()
+    main()
