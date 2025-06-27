2b110b1f4a5f1729dcbe15cb8a91d21136167f73
chinkunlim <411383025@gms.ndhu.edu.tw>
2025-06-27 16:01:14 +0800

    Reading and writing .env files

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
 # import libraries
 import os
+import logging
 import configparser
+from dotenv import load_dotenv, set_key, find_dotenv
 
 # import modules
+logger = logging.getLogger(__name__)
 
 class Config:
     def __init__(self, ini_path="config.ini"):
@@ -14,5 +17,38 @@ class Config:
         if section and self.config.has_option(section, key):
             return self.config.get(section, key)
         return default
+    
+    def load_env(self):
+        dotenv_path = find_dotenv()
+        if not dotenv_path:
+            logger.warning(f"Couldn't find .env file...")
+            return
+        
+        load_dotenv(dotenv_path)
+        logger.info(f"Read .env file successfully")
+    
+    def get_env(self):
+        config_keys = [
+            "NOTION_API_KEY",
+            "PARENT_PAGE_ID",
+            "COURSE_HUB_ID",
+            "CLASS_SESSION_ID",
+            "TAKS_DB_ID",
+            "NOTE_DB_ID",
+            "PROJECT_DB_ID",
+            "RESOURCE_DB_ID",
+        ]
+        return {key: os.getenv(key) for key in config_keys if os.getenv(key) is not None}
+
+    def set_env(self, key, value):
+        dotenv_path = find_dotenv()
+        if not dotenv_path:
+            logger.warning(f".env file not found...")
+        
+        write_env_key = set_key(dotenv_path, key, value)
+        if write_env_key:
+            logger.info(f"write value to {key} successfully...")
+        else:
+            logger.error(f"failed to write value to {key}...")
 
 config = Config()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 
 # import modules
 from .logging import setup_logging
+from .config import config
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -12,6 +13,12 @@ def main():
     logger.debug("debug")
     logger.error("error")
     logger.warning("warning")
+
+    config.load_env()
+    env = config.get_env()
+    api_key = env.get("NOTION_API_KEY")
+    print(api_key)
+    config.set_env("abc","456")
 
 if __name__ == "__main__":
     main()
